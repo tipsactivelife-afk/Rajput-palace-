c0ca0b5a11e02b147de1eb84fa57480ce34efecf
tipsactivelife-afk <tipsactivelife@gmail.com>
2026-08-26 19:15:44 +0530

    Delete robots.ts

--- robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { hotelConfig } from "@/lib/hotel-config";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${hotelConfig.siteUrl.replace(/\/$/, "")}/sitemap.xml`,
-  };
-}
